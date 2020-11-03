73242b633648c244dde7c98e11d15c3f33ef0438
Kilroy <jordans1@vt.edu>
2020-11-03 14:40:47 -0500

    added default path

--- server.js
+++ server.js
@@ -67,6 +67,11 @@ app.delete("/api/notes/:id", function(req, res) {
       });
   });
 });
+
+//set up default path
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 //set up server to listen
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
